6368e36a4b1ec610cbc659fcff270384757fa0d7
Efriem Desalew <efidesalew@gmail.com>
2022-11-20 23:05:13 +0300

    Find one

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Param, Post, Redirect, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Redirect('http://localhost:3000/cats')
   @Post()
   create(@Req() request: Request): string {
     return 'This action adds a new cat.';
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    return `This will return a cat with an id of ${params.id}`;
   }
 
   @Get()
